2c6b351bf51a66b2b6cc9ab1e71258cb57b015fc
Anupa Thapa <anupa@Anupas-MacBook-Air.local>
2023-03-26 14:27:10 +0300

    store added and combined root reducer exported

--- src/store.js
+++ src/store.js
@@ -1,12 +1,16 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { combineReducers, configureStore } from '@reduxjs/toolkit'
 import bookReducer from './features/books/bookSlice'
+import userReducer from './features/users/userSlice'
+import authorReducer from './features/authors/authorSlice'
 
-export const store = configureStore({
-  reducer: {
-    books: bookReducer,
-    users: userReducer,
-    authors: authorReducer
-  }
+const rootReducer = combineReducers({
+  books: bookReducer,
+  users: userReducer,
+  author: authorReducer
+})
+
+const store = configureStore({
+  reducer: rootReducer
 })
 
 export default store
